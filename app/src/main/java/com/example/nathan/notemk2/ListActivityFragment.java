9b19fd6e729fa0157135af9d5422c8bb4fc8b041
nathanjay787 <nathanjay787@gmail.com>
2017-09-23 12:04:06 -0400

    Captured root view

--- app/src/main/java/com/example/nathan/notemk2/ListActivityFragment.java
+++ app/src/main/java/com/example/nathan/notemk2/ListActivityFragment.java
@@ -17,6 +17,7 @@ public class ListActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View root = inflater.inflate(R.layout.fragment_list, container, false);
+        return root;
     }
 }
